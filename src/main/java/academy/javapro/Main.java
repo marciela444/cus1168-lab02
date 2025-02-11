c397a276cbffac7947b08170059cfd4e6c260b90
“Marciela444” <marciela.palaguachi21@my.stjohns.edu>
2025-02-10 23:54:35 -0500

    finished homework

--- src/main/java/academy/javapro/Main.java
+++ src/main/java/academy/javapro/Main.java
@@ -4,31 +4,50 @@ public class Main {
     public static void main(String[] args) {
         // Step 1: Create a new Tesla Model S, 2024
         // TODO: Add your code here
+        Tesla myTesla = new Tesla("Model S", 2024);
 
         // Step 2: Display initial status
         System.out.println("Initial Status:");
         // TODO: Display car's initial state
+        System.out.println(myTesla.getMake() + " " + myTesla.getModel() + " " + myTesla.getYear());
+        System.out.println("Running: " + myTesla.isRunning());
+        System.out.println("Autopilot: " + myTesla.isAutopilotEnabled());
+        System.out.println("Charging: " + myTesla.isCharging());
+
+
 
         // Step 3: Start the car and test basic vehicle functions
         System.out.println("\nTesting basic vehicle functions:");
         // TODO: Start engine
+        myTesla.startEngine();
         // TODO: Accelerate
+        myTesla.accelerate(); 
         // TODO: Brake
+        myTesla.brake(); 
 
         // Step 4: Test autonomous features
         System.out.println("\nTesting autonomous features:");
         // TODO: Enable autopilot
+        myTesla.enableAutopilot();
         // TODO: Check if autopilot is enabled
+        System.out.println("Autopilot is currently: " + (myTesla.isAutopilotEnabled() ? "enabled" : "disabled"));
         // TODO: Disable autopilot
+        myTesla.disableAutopilot();
 
         // Step 5: Test electric features
         System.out.println("\nTesting electric features:");
         // TODO: Start charging
+        myTesla.charge();
         // TODO: Check charging status
+        System.out.println("Charging status: " + myTesla.isCharging());
 
         // Step 6: Stop the car and display final status
         System.out.println("\nFinal car status:");
         // TODO: Stop engine
+        myTesla.stopEngine();
         // TODO: Display final state
+        System.out.println("Running: " + myTesla.isRunning());
+        System.out.println("Autopilot: " + myTesla.isAutopilotEnabled());
+        System.out.println("Charging: " + myTesla.isCharging());
     }
 }
--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -5,9 +5,89 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
     private boolean charging;
 
     // TODO: Create constructor that takes model and year
+    public Tesla(String model, int year){
+        super("Tesla", model, year);
+        this.autopilotEnabled = false;
+        this.charging = false;
+    }
     // TODO: Set make to "Tesla"
     // TODO: Initialize autopilotEnabled and charging to false
 
     // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
     // Each method should include appropriate print statements
+    @Override
+public void startEngine() {
+    if (!isRunning) {
+        isRunning = true;
+        System.out.println(getMake() + " " + getModel() + " started");
+    } else {
+        System.out.println(getMake() + " " + getModel() + " is already running.");
+      }
+}
+
+    @Override
+    public void stopEngine() {
+        if (isRunning) {
+            isRunning = false;
+            System.out.println(model + " engine stopped.");
+        } else {
+            System.out.println(model + " is already off.");
+        }
+    }
+
+    @Override
+    public void accelerate() {
+        if (isRunning) {
+            System.out.println(getMake() + " " + getModel() + " is accelerating...");
+        } else {
+            System.out.println("Start the engine first!");
+        }
+    }
+
+    @Override
+    public void brake() {
+        System.out.println(getMake() + " " + getModel() + " is braking...");
+    }
+
+    // Implementing Electric methods
+    @Override
+    public void charge() {
+        if (!charging) {
+            charging = true;
+            System.out.println(getMake() + " " + getModel() + " is now charging.");
+        } else {
+            System.out.println(getMake() + " " + getModel() + " is already charging.");
+        }
+    }
+
+    @Override
+    public boolean isCharging() {
+        return charging;
+    }
+
+    // Implementing Autonomous methods
+    @Override
+    public void enableAutopilot() {
+        if (!autopilotEnabled) {
+            autopilotEnabled = true;
+            System.out.println("Autopilot enabled");
+        } else {
+            System.out.println(model + " autopilot is already on.");
+        }
+    }
+
+    @Override
+    public void disableAutopilot() {
+        if (autopilotEnabled) {
+            autopilotEnabled = false;
+            System.out.println("Autopilot disabled");
+        } else {
+            System.out.println(model + "Autopilot is already off.");
+        }
+    }
+
+    @Override
+    public boolean isAutopilotEnabled() {
+        return autopilotEnabled;
+    }
 }
